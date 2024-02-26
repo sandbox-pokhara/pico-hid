9db690947bd53d5e5a07d73dab06b8f1f1bc8c69
Sakar-Pathak <47787401+sakar111@users.noreply.github.com>
2024-02-26 11:01:25 +0545

    keyboard press_and_release, press, release implementation (#2)

---
+++ pico_hid/keyboard.py
@@ -0,0 +1,29 @@
+import time
+
+from serial import Serial
+
+
+class Keyboard:
+    def __init__(self, serial_port: Serial):
+        self.serial_port = serial_port
+
+    def press(self, key: str):
+        self.send(f"keyboard_press,{key}\n")
+
+    def release(self, key: str):
+        self.send(f"keyboard_release,{key}\n")
+
+    def release_all(self):
+        self.send("keyboard_release\n")
+
+    def press_and_release(self, key: str):
+        self.send(f"keyboard_keystroke,{key}\n")
+
+    def write(self, text: str):
+        self.send(f"keyboard_write,{text}\n")
+
+    def send(self, command: str):
+        self.serial_port.write(command.encode())
+        time.sleep(0.1)  # Mandatory delay
+        echoed_message = self.serial_port.readline().decode().strip()
+        print("Echoed message:", echoed_message)
